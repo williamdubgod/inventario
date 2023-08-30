c8a5c7c90a12f6cf716710513cf99396e22b2972
Benefrancis do Nascimento <benefrancis@gmail.com>
2023-08-30 14:21:33 -0300

    Criamos o README.md, o Diagrama de Classes

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,7 +1,13 @@
 package br.com.fiap;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
     }
 }
